6892e944f274a4cebab43a3d1de75eca94f54523
Kongkiat Chongchaya <84502298+Switch47@users.noreply.github.com>
2022-04-10 16:47:05 +0700

    Add files via upload

---
+++ src/main/java/zork/object/monster/Sephiroth.java
@@ -0,0 +1,9 @@
+package zork.object.monster;
+
+public class Sephiroth extends Monster{
+
+    public Sephiroth() {
+        super("Sephiroth",100,10,10,0.6,1,50,100);
+    }
+
+}
---
+++ src/main/java/zork/object/monster/Werewolf.java
@@ -0,0 +1,9 @@
+package zork.object.monster;
+
+public class Werewolf extends Monster{
+
+    public Werewolf() {
+        super("Werewolf",100,10,10,0.6,1,50,100);
+    }
+
+}
